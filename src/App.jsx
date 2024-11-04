b3c15da208f18d7f458cfe5754b3aac5ac2222d4
Stephany Marciano Dias <130869859+stephanymdias@users.noreply.github.com>
2024-11-04 20:10:58 -0300

    Add files via upload

---
+++ src/App.jsx
@@ -0,0 +1,12 @@
+import './App.css'
+import ListaDeContatos from './components/ListaDeContatos';
+
+function App(){
+  return(
+    <div className="App">
+      <ListaDeContatos/>
+    </div>
+  );
+}
+
+export default App;
